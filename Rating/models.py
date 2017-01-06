7f442e48608275fc4e7522091351e3bcd4a024e6
himanshu <hmansusoni444@gmail.com>
2017-01-06 20:39:47 +0530

    models Initialization

--- Rating/models.py
+++ Rating/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+#from django.db.models import Avg
 # Create your models here.
+class Movies(models.Model):
+    movie_name=models.CharField(max_length=100)
+    release_date=models.DateField("date Released")
+class Tvshows(models.Model):
+    show_name=models.CharField(max_length=100)
+    season=models.IntegerField
+    episode=models.IntegerField
+    release_date=models.DateField("Release date")
+
+class Novels(models.Model):
+    novel_name=models.CharField(max_length=100)
+    writer=models.CharField(max_length=100)
+    publisher=models.CharField(max_length=100)
+    release_date = models.DateField("Release date")
+class Ratings(models.Model):
+    movie=models.ForeignKey(Movies,on_delete=models.CASCADE)
+    tvshow=models.ForeignKey(Tvshows,on_delete=models.CASCADE)
+    novels=models.ForeignKey(Novels,on_delete=models.CASCADE)
+    rating=models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
